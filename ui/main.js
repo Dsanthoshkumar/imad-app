8e79e239a12b00fb18659c2b5b7e8eeb75f56a4e
Dsanthoshkumar <30830626+Dsanthoshkumar@users.noreply.github.com>
2017-09-03 18:20:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,8 @@
 console.log('Loaded!');
+
+var element = document.getElementById('main-text');
+element.innerHTML = 'new value';
+
 var img = document.getElementById('modi');
       img.onclick = function () {
     img.style.marginLeft ='500px';
